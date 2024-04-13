8c8d14003ef6c5b6f4c41b7da17bb594f107c7b4
Leonardo Mendez <leo.mendez.9@outlook.com>
2024-04-13 14:25:29 -0500

    Add project

---
+++ RickAndMortyTest/RickAndMortyTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTestApp.swift
+//  RickAndMortyTest
+//
+//  Created by Leonardo Mendez on 13/04/24.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
